b1d94251365da0089fea1a78f121c8655120cf9e
ketanTechracers <ksaxena@gammastack.com>
2020-01-16 15:53:17 +0530

    Added views and resources

--- server.js
+++ server.js
@@ -4,9 +4,20 @@ const bodyParser = require('body-parser')
 const WeatherRoutes = require("./weather.js");
 const port = process.env.PORT || 3000;
 
+//setting views and view engine
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// adding routes and middlewares
 app.use(bodyParser.json());
 app.use("/weather", WeatherRoutes);
 app.get('/', (req, res) =>
-  res.send('<h2>My node app is running at port '+ port +'</h2>'));
+  res.render('pages/index')
+  // res.send('<h2>My Weather Bot</h2>' +
+  //   '<iframe width="350" height="430" src="https://console.api.ai/api-client/demo/embedded/ketan-saxena-weather-bot"></iframe>'
+  // )
+);
 
+//starting the App
 app.listen(port, () => console.log('app running at localhost:' + port));
