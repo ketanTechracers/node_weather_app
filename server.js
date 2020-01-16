ec6fb270ae82925163ff87694a682fb1f532ca81
ketanTechracers <ksaxena@gammastack.com>
2020-01-16 15:53:17 +0530

    route callback corrected

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use("/weather", WeatherRoutes);
 app.get('/', (req, res) =>
-  response.send('<h2>My node app is running at port '+ port +'</h2>'));
+  res.send('<h2>My node app is running at port '+ port +'</h2>'));
 
 app.listen(port, () => console.log('app running at localhost:' + port));
